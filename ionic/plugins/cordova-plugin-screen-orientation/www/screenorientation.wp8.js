45d3a5872cf2f2b944c8c418b89ca5488c4c6fc5
Felix <hello@felixtsao.com>
2016-01-25 13:07:22 -0600

    replace ionic

--- ionic/plugins/cordova-plugin-screen-orientation/www/screenorientation.wp8.js
+++
@@ -1,8 +0,0 @@
-var exec = require('cordova/exec'),
-    screenOrientation = {};
-
-screenOrientation.setOrientation = function(orientation) {
-    exec(null, null, "YoikScreenOrientation", "screenOrientation", [orientation]);
-};
-
-module.exports = screenOrientation;
